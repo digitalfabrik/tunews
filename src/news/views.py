56d172f7af765416181b58e5105fcaf5b1108f79
Sven Seeberg <mail@sven-seeberg.de>
2023-10-22 20:36:52 +0200

    Remove import view

--- src/news/views.py
+++ src/news/views.py
@@ -8,8 +8,6 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
-from basicauth.decorators import basic_auth_required
-
 from .models import NewsLanguage, NewsCategory, NewsItem
 from .tunews_clean import clean_html, get_enewsno
 
@@ -89,24 +87,3 @@ def __mapItemToResult(item):
         'content': item.content,
         'enewsno': item.enewsno,
     }
-
-@basic_auth_required(
-    target_test=lambda request: not request.user.is_authenticated
-)
-def import_news(request):
-    languages = NewsLanguage.objects.filter(wpcategory__isnull=False)
-    for language in languages:
-        posts = requests.get('https://tunewsinternational.com/wp-json/wp/v2/posts?categories={}'.format(language.wpcategory)).json()
-        for post in posts:
-            if NewsItem.objects.filter(wppostid=post["id"]):
-                continue
-            newsitem = NewsItem(
-                title=post["title"]["rendered"],
-                content=clean_html(post["content"]["rendered"]),
-                enewsno=get_enewsno(post["content"]["rendered"]),
-                pub_date=post["date"]+"+00:00",
-                language=language,
-                wppostid=post["id"]
-            )
-            newsitem.save()
-    return HttpResponse([], content_type="application/json")
