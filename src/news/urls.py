3c0c692acdc6e889fdf1a9cb4341a7ff994959a5
Sven Seeberg <mail@sven-seeberg.de>
2023-10-23 09:56:07 +0200

    Fix urls.py

--- src/news/urls.py
+++ src/news/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('tags', views.categories),
     path('languages', views.languages),
-    path('import', views.import_news, name='import_news'),
     path('<int:news_id>', views.singlenews, name='singlenews'),
     path('<str:language_code>', views.index, name='index'),
 ]
