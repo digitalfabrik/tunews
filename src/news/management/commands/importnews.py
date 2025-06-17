1e353cf71dff34f0cf071a5961a5fce7e61bc8be
JoeyStk <johannes.stock@yahoo.de>
2025-06-17 19:06:19 +0200

    Format with ruff

--- src/news/management/commands/importnews.py
+++ src/news/management/commands/importnews.py
@@ -1,13 +1,9 @@
-import json
 import requests
 
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from ...models import NewsLanguage, NewsCategory, NewsItem
-from ...tunews_clean import clean_html, get_enewsno
+from ...models import NewsLanguage, NewsItem
+from ...tunews_clean import clean_html
 
 
 class Command(BaseCommand):
@@ -16,7 +12,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         languages = NewsLanguage.objects.filter(wpcategory__isnull=False)
         for language in languages:
-            posts = requests.get('http://tuenews.de/wp-json/wp/v2/posts/?lang={}'.format(language.code)).json()
+            posts = requests.get(
+                "http://tuenews.de/wp-json/wp/v2/posts/?lang={}".format(language.code)
+            ).json()
             print(f"Got {len(posts)} posts.")
             for post in posts:
                 if not post["acf"]["integreat"]:
@@ -30,9 +28,9 @@ class Command(BaseCommand):
                     title=post["title"]["rendered"],
                     content=clean_html(post["content"]["rendered"]),
                     enewsno=post["acf"]["tun_nummer"],
-                    pub_date=post["date"]+"+00:00",
+                    pub_date=post["date"] + "+00:00",
                     language=language,
-                    wppostid=post["id"]
+                    wppostid=post["id"],
                 )
                 newsitem.save()
                 print(f"Saving {newsitem}")
