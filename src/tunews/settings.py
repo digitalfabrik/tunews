5906c7b200c8da59f6b703c520f08892133f38d8
Sven Seeberg <mail@sven-seeberg.de>
2025-06-17 18:23:47 +0200

    Disable Debug by default

--- src/tunews/settings.py
+++ src/tunews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sf!!8qec6$sx@0_rng)&jfw6ss_ff_&wq-&g%qg(7w8d2ybs&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
