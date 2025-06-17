6cebe61848c54d971b1b7324fdc1c6604b5946e6
JoeyStk <johannes.stock@yahoo.de>
2025-06-17 19:03:47 +0200

    Update to new URL

--- src/news/management/commands/importnews.py
+++ src/news/management/commands/importnews.py
@@ -11,21 +11,25 @@ from ...tunews_clean import clean_html, get_enewsno
 
 
 class Command(BaseCommand):
-    help = "Get news from tunewsinternational.com"
+    help = "Get news from tuenews.de"
 
     def handle(self, *args, **options):
         languages = NewsLanguage.objects.filter(wpcategory__isnull=False)
         for language in languages:
-            posts = requests.get('https://tunewsinternational.com/wp-json/wp/v2/posts?categories={}'.format(language.wpcategory)).json()
+            posts = requests.get('http://tuenews.de/wp-json/wp/v2/posts/?lang={}'.format(language.code)).json()
             print(f"Got {len(posts)} posts.")
             for post in posts:
+                if not post["acf"]["integreat"]:
+                    continue
+
                 if NewsItem.objects.filter(wppostid=post["id"]):
                     print(f"News {post['id']} already exists.")
                     continue
+
                 newsitem = NewsItem(
                     title=post["title"]["rendered"],
                     content=clean_html(post["content"]["rendered"]),
-                    enewsno=get_enewsno(post["content"]["rendered"]),
+                    enewsno=post["acf"]["tun_nummer"],
                     pub_date=post["date"]+"+00:00",
                     language=language,
                     wppostid=post["id"]
